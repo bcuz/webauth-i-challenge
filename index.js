4e2d87e333047d27d5732ea1991f571487f6564f
adam <bcuz@users.noreply.github.com>
2019-06-05 21:16:13 -0400

    create users table

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = express();
 
 server.use(express.json());
 
+
+
 const port = process.env.PORT || 5001;
 server.listen(port, function() {
   console.log(`\n=== API Listening on http://localhost:${port} ===\n`);
