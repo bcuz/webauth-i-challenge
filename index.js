1b69ade046bc0fa7191329e87242f8cefd70b0ce
adam <bcuz@users.noreply.github.com>
2019-06-05 21:21:42 -0400

    hit /api/users endpoint

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 
-// const Dishes = require('./data/dishes-model');
+const Users = require('./database/users-model');
 
 const server = express();
 
 server.use(express.json());
 
-
+server.get('/api/users', async (req, res) => {
+  try {
+      const users = await Users.get();
+      res.status(200).json(users);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    });
+  }
+});
 
 const port = process.env.PORT || 5001;
 server.listen(port, function() {
