7894b70849672ee9993f5be0afa1c5b7bb5a33da
adam <bcuz@users.noreply.github.com>
2019-06-05 21:07:35 -0400

    base server code

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// const Dishes = require('./data/dishes-model');
+
+const server = express();
+
+server.use(express.json());
+
+const port = process.env.PORT || 5001;
+server.listen(port, function() {
+  console.log(`\n=== API Listening on http://localhost:${port} ===\n`);
+});
